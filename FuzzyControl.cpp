ac52234e2c9e6b115e5e5088a44b22a3ae467d6b
re-high <35254074+re-high@users.noreply.github.com>
2019-07-31 20:27:23 +0800

    Add files via upload

---
+++ FuzzyControl.cpp
@@ -0,0 +1,187 @@
+#include "FuzzyControl.h"
+#include "Arduino.h"
+
+Var::Var(int numSet){
+    this->numSet = numSet;
+    //Mf *mf[numSet];
+}
+void Var::rangeSet(int rangeMin,int rangeMax) {
+    this->rangeMin = rangeMin;
+    this->rangeMax = rangeMax ;
+  }
+  void Var::membershipFunction(int mfIndex,double trimf_a,double trimf_b,double trimf_c) {
+    mf[mfIndex-1] =new Mf(trimf_a,trimf_b,trimf_c);
+  }
+  void Var::membershipFunction(int mfIndex,double trapmf_a,double trapmf_b,double trapmf_c,double trapmf_d) {
+    mf[mfIndex-1] =new  Mf(trapmf_a,trapmf_b,trapmf_c,trapmf_d);
+  }
+
+Mf::Mf(double trimf_a,double trimf_b,double trimf_c){
+  this->trimf_a=trimf_a;
+  this->trimf_b=trimf_b;
+  this->trimf_c=trimf_c;
+  this->mfFlag ="trimf";
+}
+Mf::Mf(double trapmf_a,double trapmf_b,double trapmf_c,double trapmf_d){
+  this->trapmf_a=trapmf_a;
+  this->trapmf_b=trapmf_b;
+  this->trapmf_c=trapmf_c;
+  this->trapmf_d=trapmf_d;
+  this->mfFlag = "trapmf";
+}
+double Mf::mfvalue(double input) {
+   double res;
+     if(mfFlag=="trimf"){
+      if (input<=trimf_a) {
+        return 0;
+      }
+      else if(trimf_a<input&&input<=trimf_b)
+      { 
+        res = (1/(trimf_b-trimf_a))*input-trimf_a/(trimf_b-trimf_a);
+        return res;
+      }
+      else if(trimf_b<input&&input<trimf_c) {
+        res = -(1/(trimf_c-trimf_b))*input+trimf_c/(trimf_c-trimf_b);
+        return res;
+      }
+      else {
+        return 0;
+      }
+     }
+      else if(mfFlag=="trapmf"){
+        if (input<trapmf_a) {
+          return 0;
+        }
+        else if(trapmf_a!=trapmf_b&&trapmf_a<input&&input<=trapmf_b)
+        { 
+          res = (1/(trapmf_b-trapmf_a))*input-trapmf_a/(trapmf_b-trapmf_a);
+          return res;
+        }
+        else if(trapmf_b<=input&&input<=trapmf_c) {
+          return 1;
+        }
+        else if(trapmf_c==trapmf_d&&trapmf_c<input) {
+          return 0;
+        }
+        else if(trapmf_c!=trapmf_d&&trapmf_c<input&&input<trapmf_d) {
+          res = -(1/(trapmf_d-trapmf_c))*input+trapmf_d/(trapmf_d-trapmf_c);
+          return res;
+        }else
+          return 0 ;
+      }  
+   else{
+      return 0;
+    
+    }
+  }
+  //返回重心模糊集的重心
+  //对于对称的三角隶属函数来说，就是trimf_b
+  double Mf::getcenter() {
+    return trimf_b;
+  }
+
+Rule::Rule(int var1Index,int var2Index,int var3Index){
+    this->input1Index = var1Index-1;
+    this->input2Index = var2Index-1;
+    this->outputIndex = var3Index-1;
+}
+FuzzyControl::FuzzyControl(int numSet1,int numSet2,int numSet3) {
+    this->numSet1=numSet1;
+    this->numSet2=numSet2;
+    this->numSet3=numSet3;
+    input1 = new Var(this->numSet1);
+    input2 = new Var(this->numSet2);
+    output = new Var(this->numSet3);    
+  }
+ void FuzzyControl::setRange(int input1rangeMin,int input1rangeMax,int input2rangeMin,int input2rangeMax,int outputrangeMin,int outputrangeMax) {
+    this->input1->rangeSet(input1rangeMin,input1rangeMax);
+    this->input2->rangeSet(input2rangeMin,input2rangeMax);
+    this->output->rangeSet(outputrangeMin,outputrangeMax);
+  }
+void FuzzyControl::addmf(int  varType,int mfIndex,double trimf_a,double trimf_b,double trimf_c) {
+    if(varType==1) {
+      input1->membershipFunction(mfIndex,trimf_a,trimf_b,trimf_c);
+    }
+    if(varType==2) {
+      input2->membershipFunction(mfIndex,trimf_a,trimf_b,trimf_c);
+    }
+    if(varType==3) {
+      output->membershipFunction(mfIndex,trimf_a,trimf_b,trimf_c);
+    }
+  }
+ void FuzzyControl::addmf(int  varType,int mfIndex,double trapmf_a,double trapmf_b,double trapmf_c,double trapmf_d) {
+    if(varType==1) {
+      input1->membershipFunction(mfIndex,trapmf_a,trapmf_b,trapmf_c,trapmf_d);
+    }
+    if(varType==2) {
+      input2->membershipFunction(mfIndex,trapmf_a,trapmf_b,trapmf_c,trapmf_d);
+    }
+    if(varType==3) {
+      output->membershipFunction(mfIndex,trapmf_a,trapmf_b,trapmf_c,trapmf_d);
+    }
+  }
+void FuzzyControl::setrulenum(int num) {
+    this->rulenum = num;
+    //this->rule = new Rule[rulenum]; 
+  }
+  //计算输出数值
+  //传入数值：input1和input2
+ double FuzzyControl::caculate(double value1,double value2) {
+    this->degreeFiringRule(value1,value2);
+    this->returnCenter();
+    this->getTrapezoid();
+    return this->defuzzification();
+    
+  }
+
+  void FuzzyControl::addrule(int ruleIndex,int var1Index,int var2Index,int var3Index) {
+    rule[ruleIndex-1] = new Rule(var1Index,var2Index,var3Index);
+  }
+  //计算每条规则的激活度
+  void FuzzyControl::degreeFiringRule(double x1,double x2){
+    for(int i = 0;i<rulenum;i++) {
+      double tempinput1 = input1->mf[rule[i]->input1Index]->mfvalue(x1);
+      double tempinput2 = input2->mf[rule[i]->input2Index]->mfvalue(x2);
+      if(tempinput1<tempinput2) {
+        rule[i]->degree = tempinput1;
+      }
+      else {
+        rule[i]->degree = tempinput2;
+      }
+      if(rule[i]->degree!=0) {
+        rule[i]->flag=true;
+      }
+      else {
+        rule[i]->flag = false;
+      }
+    }
+  }
+  //计算每条规则对应的模糊输出集的重心
+  void FuzzyControl::returnCenter() {
+    for(int i = 0;i<rulenum;i++) {
+    rule[i]->center = output->mf[rule[i]->outputIndex]->getcenter();
+    }
+  }
+  //计算梯形面积
+  void FuzzyControl::getTrapezoid() {
+    for(int i = 0;i<rulenum;i++) {
+      if(rule[i]->flag==true) {
+    rule[i]->trapezoid =  (output->mf[rule[i]->outputIndex]->trimf_c-output->mf[rule[i]->outputIndex]->trimf_a)*(rule[i]->degree-(rule[i]->degree*rule[i]->degree)/2);
+    }
+      else {
+        rule[i]->trapezoid = 0;
+      }
+    }
+  }
+  double FuzzyControl::defuzzification() {
+    double sum1=0;  //分子
+    double sum2=0;  //分母
+    for(int i = 0;i<rulenum;i++) {
+      if(rule[i]->flag==true) {
+        sum1 =sum1+ (rule[i]->center)*(rule[i]->trapezoid);
+        sum2 = sum2 + rule[i]->trapezoid;
+      }
+    }
+    return (sum1/sum2);
+  }
+  
---
+++ motorPosition/motorPosition.ino
@@ -0,0 +1,142 @@
+/*
+  Arduino通过FuzzyControl库构造一个双输入单输出的模糊控制器，控制直流电机追踪到理想的位置（本程序中理想位置设置的为正弦信号）
+  by 梁悦（https://re-high.github.io/）
+ 
+  如需获得本示例程序详细电路信息以及如何使用FuzzyControl库的更多知识，请参考我的博客：
+   https://re-high.github.io/
+ 
+*/
+#include <FuzzyControl.h>
+//初始化一个双输入单输出的模糊控制器
+//三个输入参数分别是输入量1、输入量2、输出量的模糊子集个数
+FuzzyControl fuzzyController(3,3,5);
+int fuzhi = 50;              //fuzhi：正弦波的幅值
+#define ENCODER_A_PIN 2      //直流电机编码器的A相连接引脚
+#define ENCODER_B_PIN 3      //直流电机编码器的B相连接引脚
+#define motor1 8             //电机
+#define motor2 9             //电机
+#define ENA 5                //使能引脚
+#define STBY 12
+//定义模糊子集名称及其对应的索引
+#define N 1
+#define Z 2
+#define P 3
+#define NB 1
+#define NS 2
+#define ZZ 3
+#define PS 4
+#define PB 5
+long position;               //编码器每获取一个脉冲，position+1
+double position_actual;      //当前位置
+double error_last = 0;              
+long   preTime=0;
+
+
+    
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(2000000);
+   //setRange函数设置输入输出论域，输入范围为[-20，20]，输出范围为[-255，255]
+   fuzzyController.setRange(-20,20,-20,20,-255,255);   
+   /*addmf函数添加隶属度函数
+    * 第一个参数表示变量类型（1表示input1，2表示input2，3表示input3）
+    * 第二个参数表示模糊子集索引（模糊子集名称）
+    * 后面的参数是三角型或梯形隶属度函数的参数    */
+   fuzzyController.addmf(1,N,-20,-20,-5,0);
+   fuzzyController.addmf(1,Z,-3,0,3);
+   fuzzyController.addmf(1,P,0,5,20,20);
+   fuzzyController.addmf(2,N,-20,-20,-5,0);
+   fuzzyController.addmf(2,Z,-3,0,3);
+   fuzzyController.addmf(2,P,0,5,20,20);
+   fuzzyController.addmf(3,NB,-255,-180,-100);
+   fuzzyController.addmf(3,NS,-200,-110,-20);
+   fuzzyController.addmf(3,ZZ,-75,0,75);
+   fuzzyController.addmf(3,PS,20,110,200);
+   fuzzyController.addmf(3,PB,100,180,255);
+   //setrulenum函数设置模糊控制规则总数
+   fuzzyController.setrulenum(9); 
+   //依次添加控制规则，第一次输入参数表示当前输入的是第几条规则
+   //后面三个参数分别为input1、input2、output所属的模糊子集索引（模糊子集名称）
+   fuzzyController.addrule(1,N,N,NB);  
+   fuzzyController.addrule(2,N,Z,NS); 
+   fuzzyController.addrule(3,N,P,ZZ); 
+   fuzzyController.addrule(4,Z,N,NS); 
+   fuzzyController.addrule(5,Z,Z,ZZ); 
+   fuzzyController.addrule(6,Z,P,PS); 
+   fuzzyController.addrule(7,P,N,ZZ); 
+   fuzzyController.addrule(8,P,Z,PS);
+   fuzzyController.addrule(9,P,P,PB); 
+  pinMode(motor1, OUTPUT);                          //电机引脚设置
+  pinMode(motor2, OUTPUT); 
+  pinMode(ENA,OUTPUT);
+  pinMode(STBY,OUTPUT);
+  digitalWrite(STBY, HIGH);   
+  pinMode(ENCODER_A_PIN, INPUT);
+  pinMode(ENCODER_B_PIN, INPUT);
+  attachInterrupt(0, read_quadrature, CHANGE);           //设置外部中断
+
+
+     
+}
+
+void loop() {
+unsigned long now = millis();   //获取当前时间
+float position_ideal=fuzhi*sin(0.1*3.14*now/1000);  //产生正弦波
+float TimeCh = (now-preTime)/1000.0;  
+float SampleTime = 0.05;       //设置采样时间
+if (TimeCh>=SampleTime)
+{
+ position_actual =(double) position/130;    //标准化当前位置，和编码器的线数有关，这里的position_actual单位是圈数
+ double error = position_actual - position_ideal;        //计算当前时刻的追踪偏差
+ double  DTerm = (error-error_last)/TimeCh;               //计算偏差变化率
+ error = constrain(error,-20,20);  //避免输入量1超过设置的论域，对输入量1进行限幅
+ DTerm = constrain(DTerm,-20,20);  //避免输入量2超过设置的论域，对输入量2进行限幅
+int  u = (int)fuzzyController.caculate(error,DTerm);  // 计算模糊控制器的输出量
+u = constrain(u,-255,255);  //对输出量限幅
+dianji(u);    //把计算得到的控制量传给电机转动函数
+error_last = error;   //把当前控制周期的追踪偏差赋值给error_last，给下个控制周期计算用
+preTime = now;        //把当前控制周期的时刻值赋值给preTime，给下个控制周期计算用
+serialprint(position_ideal,position_actual);  //串口监控函数
+}
+}
+/**************************************************************************
+函数功能：外部中断读取编码器数据
+**************************************************************************/
+void read_quadrature(){  
+  // found a low-to-high on channel A ENA脚下降沿中断触发
+  if (digitalRead(ENCODER_A_PIN) == LOW){   
+    // check channel B to see which way 查询ENB的电平以确认是顺时针还是逆时针旋转
+    if (digitalRead(ENCODER_B_PIN) == LOW)
+      position++;
+  }
+  // found a high-to-low on channel A ENA脚上升沿中断触发
+  else{
+    // check channel B to see which way 查询ENB的电平以确认是顺时针还是逆时针旋转
+    if (digitalRead(ENCODER_B_PIN) == LOW)
+      position--;
+  }
+} 
+/**************************************************************************
+函数功能：电机转动
+入口参数：控制量u
+**************************************************************************/
+void dianji(int u){
+  if(u<0){
+    digitalWrite(motor1, HIGH);  //设置电机转动方向
+    digitalWrite(motor2, LOW);  
+    analogWrite(ENA,abs(u));     //把PWM绝对值传入使能引脚，让电机转动
+  }
+  else{
+     digitalWrite(motor1, LOW);  
+     digitalWrite(motor2, HIGH);  
+     analogWrite(ENA,abs(u));  
+  }
+}
+/**************************************************************************
+函数功能：串口输出当前时刻理想的位置信号和当前位置
+**************************************************************************/
+void serialprint(float position_ideal,double position_actual){
+Serial.print(position_ideal); 
+Serial.print(" "); 
+Serial.println(position_actual);
+}
